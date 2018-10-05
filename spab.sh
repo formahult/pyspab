425745d903cb568b29372a7a03a281353dbd6334
Erin Dillinger <21108324@student.uwa.edu.au>
2018-10-05 14:34:17 +0800

    don't background

--- spab.sh
+++ spab.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-until (/home/pi/pyspab/spab.py --device /dev/ttyAMA0 --modem /dev/ttyUSB0 >> /tmp/spab.log &); do
+until (/home/pi/pyspab/spab.py --device /dev/ttyAMA0 --modem /dev/ttyUSB0 >> /tmp/spab.log); do
 	echo "Server 'spab.py' crashed with exit code $?. Respawning.."
 	sleep 5
 done
